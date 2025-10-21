216a0ca2f9079b5fadb6d60fed6a6d8fa7410afb
Ester <esterdasilvabertolani@gmail.com>
2025-10-21 10:23:39 -0300

    implement player methods
    Implemented methods for attacking, defending, and checking if the player is alive.

--- Jogo_de_Cartas_C2/Jogador.java
+++ Jogo_de_Cartas_C2/Jogador.java
@@ -1,6 +1,6 @@
 package Trabalhos_Avaliações.Jogo_de_Cartas_C2;
 
-public class Jogador {
+public class Jogador implements iJogavel {
 
     private String nome;
     private int vida;
@@ -24,6 +24,43 @@ public class Jogador {
         return defesa;
     }
 
-    
+    /* ====================================================================================== */
+
+    // Métodos:
+// • atacar → recebe o poder e não retorna nada.
+// Esse método, quando implementado, reduz a defesa do jogador atacado (inimigo);
+// se a defesa cair abaixo de zero, perde 1 ponto de vida e a defesa é aumentada em
+// +100. Por exemplo, se a defesa cair para -20, deve aumentar em 100 e ir para 80.
+
+// • defender(int poder) → recebe o poder e não retorna nada
+// Esse método aumenta a defesa do jogador
+
+// • estaVivo → que retorna um booleano
+// Esse método retorna true se a vida ou a defesa forem maiores que zero.
+
+    @Override
+    public void atacar(int poder) {
+        int vida = this.getVida(), defesa = this.getDefesa() - poder;
+
+        if (defesa < 0){
+            defesa += 100;
+            vida--;
+        }
+        
+    }
+
+    @Override
+    public void defender(int poder) {
+        int def = this.getDefesa() + poder;
+    }
+
+    @Override
+    public boolean vivo() {
+        if ((this.getVida() > 0) || (this.getDefesa() > 0)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
 }
