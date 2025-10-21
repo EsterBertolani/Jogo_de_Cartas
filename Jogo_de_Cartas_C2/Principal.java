7f78ce4efe45c70409fce5d0fc4bc454ec3fab7d
Ester <esterdasilvabertolani@gmail.com>
2025-10-21 11:05:05 -0300

    Implement initial game structure in Principal.java
    Added initial structure and comments for game logic.

--- Jogo_de_Cartas_C2/Principal.java
+++ Jogo_de_Cartas_C2/Principal.java
@@ -1,9 +1,43 @@
 package Trabalhos_Avaliações.Jogo_de_Cartas_C2;
 
 public class Principal {
-
     public static void main(String[] args) {
-        
+
     }
+
+    // a) Cadastro Inicial
+    // • Criar:
+    // • 2 jogadores com nomes distintos.
+    // • 4 cartas de ataque com poderes: 10, 20, 30 e 40.
+    // • 3 cartas de defesa com poderes: 10, 15 e 20.
+    
+    public static void cadastrar() {
+
+    }
+
+    // b) Laço Principal do Jogo
+    // Executar enquanto houver mais de um jogador vivo e a quantidade de cartas
+    // jogadas for
+    // menor ou igual a 30.
+    // Dentro do laço:
+    // 1. Cada jogador vivo recebe uma carta aleatória, que pode ser de defesa ou de
+    // ataque.
+    // 2. O jogador 1 executa sua jogada contra o jogador 2.
+    // 3. Se o jogador 2 ainda estiver vivo, ele também joga.
+    // 4. Exibir o resultado parcial da rodada, mostrando:
+    // • Nome do jogador,
+    // • Nome do inimigo,
+    // • Tipo de carta jogada,
+    // • Situação atual de vida e defesa de cada um.
+
+    // c) Fim do Jogo - o jogo se encerra quando:
+    // • houver apenas um jogador vivo → ele é o vencedor.
+    // • ou quando as 30 cartas forem jogadas → vence o jogador com:
+    // • maior número de vidas;
+    // • em caso de empate, o de maior defesa.
+    // d) Exibir Resultado Final
+    // • Mostrar a lista de jogadores com suas vidas e defesas finais.
+    // • Exibir o número total de cartas jogadas.
+    // • Indicar o vencedor.
 
 }
